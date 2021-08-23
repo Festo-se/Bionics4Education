129a736de6fd3b5dbb5207ae791bd0d35c055096
Philipp <59870367+pm-b4e@users.noreply.github.com>
2021-08-23 16:29:34 +1000

    adding flash file
    with the batch file you can flash the new Bionics Kit software v.2.0.0 on your microcontroller

---
+++ bionic_kit/flash_file_batch/Bionics-Kit_Software_v.2.0.0.bat
@@ -0,0 +1,55 @@
+@ECHO OFF
+SETLOCAL EnableDelayedExpansion
+
+REM Flower Flash-Tool
+
+
+echo Flower Flash-Tool Bionics-Kit_Software_v.2.0.0
+echo PLEASE DISCONNECT ALL OTHER USB-TO-SERIAL DEVICES
+echo.
+
+pause
+echo.
+
+SET device_name=Silicon Labs
+
+:loop
+echo searching for COM port. . .
+echo.
+SET res=-1 
+
+SET exclude_1=''
+SET exclude_2=''
+SET exclude_3=''
+
+FOR /F "tokens=*" %%A IN ('WMIC path win32_pnpentity get ClassGuid^,Caption^| find "{4d36e978-e325-11ce-bfc1-08002be10318}" ^| findstr "(COM[0-9])*" ^| find "!device_name!"') DO (
+
+   SET var=%%A
+
+    FOR /F "TOKENS=2 DELIMS=(" %%a IN ("!var!") DO (
+        SET var2=%%a
+    )
+
+    FOR /F "TOKENS=1 DELIMS=)" %%a IN ("!var2!") DO (
+        SET var3=%%a
+    )
+
+    REM echo !var3!
+
+    REM flash parallel
+    CMD /c "tools\esptool.exe --chip esp32 --port !VAR3! erase_flash
+    CMD /c "tools\esptool.exe --chip esp32 --port !VAR3! --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0x0000 bin\Bionics-Kit_Software_v.2.0.0.bin" 
+    set res=1
+
+)
+
+if %res% == -1 (
+    echo No Flower found
+    echo.
+)
+
+echo.
+echo Press ENTER for continue or CLOSE the window to leave the application
+echo.
+pause
+goto loop
